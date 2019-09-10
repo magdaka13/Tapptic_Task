80453ab9e73ae95d2019c0007e0f4bea43e94fb0
Magdalena Grzeskoexit <magdalenagrzesko@gmail.com>
2019-09-10 21:47:08 +0200

    initial commit

--- app/src/main/java/com/mg/tapptic_task/ItemActivity.java
+++ app/src/main/java/com/mg/tapptic_task/ItemActivity.java
@@ -14,7 +14,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
-public class ItemActivity extends Activity implements RemoteCallListener{
+public class ItemActivity extends Activity implements RemoteCallListener {
     private TextView mItemTextView;
     private ImageView mItemImageView;
     public ArrayList<Item> items;
@@ -28,51 +28,45 @@ public class ItemActivity extends Activity implements RemoteCallListener{
         mItemTextView = (TextView) findViewById(R.id.textView);
         mItemImageView = (ImageView) findViewById(R.id.imageView);
 
-        items=new ArrayList<Item>();
+        items = new ArrayList<Item>();
 
 
         Bundle bundle = getIntent().getExtras();
-        Log.e("DetailActivity",bundle.toString());
+        Log.e("DetailActivity", bundle.toString());
 
-        if (bundle!=null) {
-            String item_id=(String)bundle.get("Item_id");
+        if (bundle != null) {
+            String item_id = (String) bundle.get("Item_id");
             DownloadData_from2URL mAsync = new DownloadData_from2URL(this);
             mAsync.execute(item_id);
 
-        }
-        else
-        {
+        } else {
             mItemTextView.setText("No text aviable");
         }
-
 
 
     }
 
     @Override
     public void onRemoteCallComplete(ArrayList<Item> arrayList) {
-    if (arrayList!=null) {
-    items.addAll(arrayList);
+        if (arrayList != null) {
+            items.addAll(arrayList);
 
-    mItemTextView.setText(items.get(0).getmText());
-    new DownloadImageTask(mItemImageView).execute(items.get(0).getmImage());
-      }
-      else
-    {
-        showErrorScreen();
-    }
+            mItemTextView.setText(items.get(0).getmText());
+            new DownloadImageTask(mItemImageView).execute(items.get(0).getmImage());
+        } else {
+            showErrorScreen();
+        }
     }
 
-    private void showErrorScreen(){
-        Toast.makeText(this,"error loading data-trying to reconnect",Toast.LENGTH_LONG).show();
+    private void showErrorScreen() {
+        Toast.makeText(this, "error loading data-trying to reconnect", Toast.LENGTH_LONG).show();
 
-        if (isConnected())
-        {
+        if (isConnected()) {
             Bundle bundle = getIntent().getExtras();
-            Log.e("DetailActivity",bundle.toString());
+            Log.e("DetailActivity", bundle.toString());
 
-            if (bundle!=null) {
-                String item_id=(String)bundle.get("Item_id");
+            if (bundle != null) {
+                String item_id = (String) bundle.get("Item_id");
                 DownloadData_from2URL mAsync = new DownloadData_from2URL(this);
                 mAsync.execute(item_id);
 
@@ -82,14 +76,13 @@ public class ItemActivity extends Activity implements RemoteCallListener{
 
     }
 
-    public boolean isConnected(){
+    public boolean isConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 
         try {
             info = cm.getActiveNetworkInfo();
-        }catch(Exception e)
-        {
-            Log.e("ItemActivity","Cannot retrieve ActiveNetworkInfo"+e.getMessage());
+        } catch (Exception e) {
+            Log.e("ItemActivity", "Cannot retrieve ActiveNetworkInfo" + e.getMessage());
         }
 
         return info != null && info.isConnectedOrConnecting();
@@ -97,26 +90,24 @@ public class ItemActivity extends Activity implements RemoteCallListener{
 
 
     class DownloadData_from2URL extends AsyncTask<String, Void, ArrayList<Item>> {
-        RemoteCallListener listener=null;
+        RemoteCallListener listener = null;
 
         public DownloadData_from2URL(RemoteCallListener listener) {
-            this.listener=listener;
+            this.listener = listener;
         }
 
 
         @Override
         protected ArrayList<Item> doInBackground(String... strings) {
-try {
-    return NetworkUtils.parseJSON_secondUrl(strings[0]);
-}catch(Exception e)
-            {
-                Log.e("ItemActivity","doInbackground:"+e.getMessage());
+            try {
+                return NetworkUtils.parseJSON_secondUrl(strings[0]);
+            } catch (Exception e) {
+                Log.e("ItemActivity", "doInbackground:" + e.getMessage());
             }
             return null;
         }
 
-        protected void onPostExecute(ArrayList<Item> result)
-        {
+        protected void onPostExecute(ArrayList<Item> result) {
 
             listener.onRemoteCallComplete(result);
         }
